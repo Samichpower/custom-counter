851afb168b662cc2deb448f2125e57b4dbd5b993
Austin <103972153+Samichpower@users.noreply.github.com>
2024-02-25 08:59:36 -0600

    Append counter inputs to the page

--- script.js
+++ script.js
@@ -3,15 +3,27 @@ const counters = {};
 const newCounterForm = document.getElementById('new-counter');
 newCounterForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  
-  const counterInput = document.getElementById('counter-input');
-  if (counterInput.value === '') return;
-  for (let key in counters) {
-    if (counterInput.value === key) return;
-  }
-  counters[counterInput.value] = 0;
 
+  const counterInput = document.getElementById('counter-input');
+  const counterName = counterInput.value
+  
+  if (counterName === '') return;
+  for (let key in counters) {
+    if (counterName === key) return;
+  }
+
+  counters[counterName] = 0;
   counterInput.value = '';
 
   console.log(counters);
-})
+
+  appendCounters(counterName);
+});
+
+function appendCounters(name) {
+  const counterContainer = document.getElementById('counter-container');
+  const newListItem = document.createElement('li');
+
+  newListItem.textContent = name;
+  counterContainer.appendChild(newListItem);
+}
